71efec225381bdbf373334207a3e84cb357c00f3
progmamun <mamun333089@gmail.com>
2026-07-27 14:31:44 +0600

    added-ascii

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -3,11 +3,19 @@ import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-  res.status(StatusCodes.NOT_FOUND).json({
-    success: false,
-    message: 'API Not Found !!',
-    error: '',
-  });
+  res.status(StatusCodes.NOT_FOUND).type('text/plain').send(String.raw`
+404 - Page not found
+
+
+
+ /\_/\\
+( o.o )  Where am I?
+ > ^ <
+  
+
+
+ 
+`);
 };
 
 export default notFound;
